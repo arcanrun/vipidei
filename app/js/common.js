d22c141d934c9e0ec329711ca988dff5e3fac917
Arkadiy Nazin <arcan.run@gmail.com>
2017-10-21 00:19:05 +0300

    index-section-3

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
 $(function() {
-
-	// Custom JS
+	$(document).on("click", function(){
+		$("div.drop-down-orang-blc").removeClass("active-blc");
+		$(".dropdown-toggle").removeClass("active-color-dropdown-toggle");
+		console.log("clicked");
+	});
+	$(".dropdown-toggle").on("click", function(){
+		// if(!$(".dropdown-toggle").parents("div.dropdown").hasClass("open"))
+		// {
+		// 	$(".drop-down-orang-blc").removeClass("active-blc");
+		// }
+		$(".drop-down-orang-blc").removeClass("active-blc");
+		$(this).parents(".drop-down-orang-blc").toggleClass("active-blc");
+		$(this).toggleClass("active-color-dropdown-toggle");
+		// 333333
+	});
 
 });
